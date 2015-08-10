d74127b07bdbc95e039b46dc9af6861de6a67ebe
Andrew Crozier <wacrozier@gmail.com>
2015-08-11 00:36:31 +0200

    Adding basic login and profile display/edit functionality

--- iaestenow/app.py
+++ iaestenow/app.py
@@ -5,8 +5,11 @@ Create a flask app serving the webpage and JSON APIs.
 import flask
 from iaestenow import geocode as geo
 from iaestenow import entries as entr
-from iaestenow.login import loginmanager, load_user, register_user
+from iaestenow.login import loginmanager, register_user
+from flask.ext import login as fl
 from iaestenow import forms
+from iaestenow.database import Session, User
+from sqlalchemy.orm.exc import NoResultFound
 
 # Create the flask app
 app = flask.Flask(__name__)
@@ -37,3 +40,44 @@ def register():
         register_user(form.email.data, form.password.data, form.name.data)
         flask.flash('Registration successful.')
     return flask.render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = forms.LoginForm(flask.request.form)
+
+    if flask.request.method == 'POST' and form.validate():
+        session = Session()
+        query = session.query(User).filter_by(email=form.email.data)
+
+        try:
+            fl.login_user(query.one())
+        except NoResultFound:
+            flask.flash('No user account with those credentials found.')
+        else:
+            flask.flash('Logged in successfully.')
+
+            next = flask.request.args.get('next')
+            #if not next_is_valid(next):
+            #    return flask.abort(400)
+
+            return flask.redirect(next or flask.url_for('index'))
+
+    return flask.render_template('login.html', form=form)
+
+@app.route('/profile')
+@fl.login_required
+def profile_edit():
+    form = forms.ProfileForm(flask.request.form, fl.current_user)
+    #if flask.request.method == 'POST' and form.validate():
+        
+    return flask.render_template('profile-edit.html', form=form)
+
+@app.route('/profile/<int:user_id>')
+def profile(user_id):
+    session = Session()
+    query = session.query(User).filter_by(id=int(user_id))
+    try:
+        user = query.one()
+    except NoResultFound:
+        flask.abort(404)
+    return flask.render_template('profile.html', user=user)
--- iaestenow/forms.py
+++ iaestenow/forms.py
@@ -6,3 +6,11 @@ class RegistrationForm(Form):
     email    = StringField('Email', [validators.Email()])
     password = PasswordField('Password', [validators.Length(min=6)])
     name     = StringField('Full Name', [validators.InputRequired()])
+
+class LoginForm(Form):
+    email    = StringField('Email', [validators.Email()])
+    password = PasswordField('Password', [validators.Length(min=6)])
+
+class ProfileForm(Form):
+    name     = StringField('Full Name', [validators.InputRequired()])
+    password = PasswordField('Password', [validators.Length(min=6)])
